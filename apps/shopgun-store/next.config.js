2fdb8ef217fcc4571c13f07b3e0d52f8beec9b9c
Mateusz Gruźla <mateusz.gruzla@sobre.pl>
2021-10-13 21:58:02 +0200

    chore: disable eslint on next.config.js @shopgun/store

--- apps/shopgun-store/next.config.js
+++ apps/shopgun-store/next.config.js
@@ -1,14 +1,13 @@
-const withPlugins = require("next-compose-plugins");
-const withTM = require("next-transpile-modules")(["@shopgun/ui-kit"],{
-    resolveSymlinks: true,
-    debug: false,
-  }
-);
-const withImages = require("next-images");
-
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['@shopgun/ui-kit'], {
+  resolveSymlinks: true,
+  debug: false,
+});
+const withImages = require('next-images');
 
 /** @type {import('next').NextConfig} */
 module.exports = withPlugins([withTM(), withImages], {
+  // eslint-disable-next-line
   webpack: (config) => {
     // custom webpack config
     return config;
