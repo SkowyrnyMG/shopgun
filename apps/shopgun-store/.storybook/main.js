6eabb368264cc69de194c1cc28df53f6d98e111a
Mateusz Gruźla <mateusz.gruzla@sobre.pl>
2021-10-25 15:10:03 +0200

    chore: configure Storybook to accept modules
    Add additional webpack extension in .storybook/main.js
    
    Install additional Storybook plugins to convert sass and scss modules.
    
    Update Component andStories handlebar.
    
    Update .babelrc in @shopgun/store

--- apps/shopgun-store/.storybook/main.js
+++ apps/shopgun-store/.storybook/main.js
@@ -1,4 +1,19 @@
+const path = require("path");
+
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: ["../src/**/*.stories.[tj]sx"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    `@storybook/preset-scss`,
+  ],
+
+  webpackFinal: async (config) => {
+    config.resolve.modules = [
+      ...(config.resolve.modules || []),
+      path.resolve(__dirname, "../src"),
+    ];
+
+    return config;
+  },
 };
