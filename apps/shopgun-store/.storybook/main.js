c6578843e68ec08b200c9c10052553db3cb445e6
Mateusz Gruźla <mateusz.gruzla@sobre.pl>
2021-10-26 12:31:18 +0200

    chore: eslint, prettier, storybook updates
    Install additional eslint/prettier dependencies to all workspaces.
    
    Install prettier into all workspaces.
    
    Extend base eslint config by prettier.
    
    Remove all prettier.rc.js files - use one global instead.
    
    Remove storybook.base.js and replace it with regular config in each
    main.js through all workspaces. Unfortunately base config caused bug and
    storybook coulnd't handle to read it.

--- apps/shopgun-store/.storybook/main.js
+++ apps/shopgun-store/.storybook/main.js
@@ -1,5 +1,12 @@
-const baseConfig = require("../../../storybook.base");
+const path = require('path');
 
 module.exports = {
-  ...baseConfig,
+	stories: ['../src/**/*.stories.tsx'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', `@storybook/preset-scss`],
+
+	webpackFinal: async (config) => {
+		config.resolve.modules = [...(config.resolve.modules || []), path.resolve(__dirname, '../src')];
+
+		return config;
+	},
 };
